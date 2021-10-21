f275832132bb4392eff8148eb8067b78a5bcd9e9
Smriti <smriti.meenu@gmail.com>
2021-10-21 19:42:04 -0400

    Modified sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum {
     public static void main(String[] args){
         int a = 1;
         int b = 2;
-        System.out.println(a+b);
+        System.out.println("The sum is: " + (a+b));
     }
 }
